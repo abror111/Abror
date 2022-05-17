3957a23e4ea1418f46ddf9c8ef961bbf7d09bf91
abror <taniyevabror02@gmail.com>
2022-05-17 22:30:44 +0300

    dasturchi

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let searchForm=document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick=()=>{
+                    searchForm.classList.toggle('active');               
+}
+let shopping=document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick=()=>{
+                    shopping.classList.toggle('active');               
+}
+let login=document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick=()=>{
+                    login.classList.toggle('active');               
+}
+
+
